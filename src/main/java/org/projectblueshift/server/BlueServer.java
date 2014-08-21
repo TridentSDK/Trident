96d62a3261dd75f98c29b55022923ff6405f0d27
Harry Fox <harry.fox.ic@icloud.com>
2014-08-21 16:35:41 +0100

    Add netty and update BlueServer

--- src/main/java/org/projectblueshift/server/BlueServer.java
+++ src/main/java/org/projectblueshift/server/BlueServer.java
@@ -1,6 +1,33 @@
 package org.projectblueshift.server;
 
+import io.netty.bootstrap.ServerBootstrap;
 import org.projectblueshift.api.Server;
 
+import java.net.InetSocketAddress;
+
 public class BlueServer implements Server {
+
+	private InetSocketAddress address;
+	private ServerBootstrap bootstrap;
+
+	public BlueServer(InetSocketAddress address) {
+		this.address = address;
+	}
+
+	public InetSocketAddress getSocketAddress() {
+		return address;
+	}
+
+	public short getPort() {
+		return (short) address.getPort();
+	}
+
+	public void startup() {
+
+	}
+
+	public void shutdown() {
+
+	}
+
 }
---
+++ src/main/java/org/projectblueshift/server/BlueStart.java
@@ -0,0 +1,13 @@
+package org.projectblueshift.server;
+
+public class BlueStart {
+
+	public static void main(String[] args) {
+		/*
+		 check some args here, using an interpreter
+		 parse the configuration file
+		 create the server from the args/config values
+		 */
+	}
+
+}
