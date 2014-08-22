99d65f3f5e789da2d53b94b6d9a6e350fad53a78
Mazen K <email@mazenmc.io>
2014-08-22 16:49:15 +0400

    Set options, doing this just to update repo.

--- src/main/java/org/projectblueshift/server/channel/BlueServerChannel.java
+++ src/main/java/org/projectblueshift/server/channel/BlueServerChannel.java
@@ -2,11 +2,13 @@ package org.projectblueshift.server.channel;
 
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 
 public class BlueServerChannel extends ChannelInitializer {
 
     @Override
     protected void initChannel(Channel channel) throws Exception {
-        // TODO: Finish this method.
+        channel.config().setOption(ChannelOption.IP_TOS, 24);
+        channel.config().setOption(ChannelOption.TCP_NODELAY, false);
     }
 }
