8ce6b2803c3821ecb263e535d8a211c8b2cfc5c8
Mazen K <email@mazenmc.io>
2014-08-22 16:40:43 +0400

    Started Netty work

--- src/main/java/org/projectblueshift/server/BlueServer.java
+++ src/main/java/org/projectblueshift/server/BlueServer.java
@@ -1,13 +1,20 @@
 package org.projectblueshift.server;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 import org.projectblueshift.api.Server;
+import org.projectblueshift.server.channel.BlueServerChannel;
 
 import java.net.InetSocketAddress;
 
 public class BlueServer implements Server {
 
     private InetSocketAddress address;
+
+    private final NioEventLoopGroup loopGroup = new NioEventLoopGroup(0,
+            (new ThreadFactoryBuilder()).setNameFormat("Netty IO #%d").setDaemon(true).build());
     private ServerBootstrap bootstrap;
 
     public BlueServer(InetSocketAddress address) {
@@ -22,8 +29,11 @@ public class BlueServer implements Server {
         return (short) address.getPort();
     }
 
-    public void startup() {
+    public void init() {
+        bootstrap = new ServerBootstrap();
 
+        bootstrap.channel(NioServerSocketChannel.class).group(loopGroup)
+                .localAddress(address.getAddress(), address.getPort()).bind().syncUninterruptibly();
     }
 
     public void shutdown() {
---
+++ src/main/java/org/projectblueshift/server/channel/BlueServerChannel.java
@@ -0,0 +1,12 @@
+package org.projectblueshift.server.channel;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+
+public class BlueServerChannel extends ChannelInitializer {
+
+    @Override
+    protected void initChannel(Channel channel) throws Exception {
+        // TODO: Finish this method.
+    }
+}
