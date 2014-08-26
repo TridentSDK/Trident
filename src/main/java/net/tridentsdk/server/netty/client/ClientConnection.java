ac8fcb61ba36dfc0ae5206c77812f1727515e0f4
AgentTroll <woodyc40@gmail.com>
2014-08-26 16:00:56 -0700

    Update ClientConnection.java

--- src/main/java/net/tridentsdk/server/netty/client/ClientConnection.java
+++ src/main/java/net/tridentsdk/server/netty/client/ClientConnection.java
@@ -20,7 +20,6 @@ package net.tridentsdk.server.netty.client;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
-import net.tridentsdk.api.docs.AccessNoDoc;
 import net.tridentsdk.server.netty.packet.Packet;
 import net.tridentsdk.server.netty.protocol.Protocol;
 
@@ -28,9 +27,12 @@ import java.net.InetSocketAddress;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-@AccessNoDoc
-class ClientConnection {
-    // TODO look over non-public, because there will be loads of crap going if we let people screw with this (easily)
+/**
+ * Handles the connection of a player upon joining
+ * 
+ * @author The TridentSDK Team
+ */ 
+public class ClientConnection {
     // TODO: Find a more efficient way to do this
     private static final Map<InetSocketAddress, ClientConnection> clientData =
             new ConcurrentHashMap<>();
@@ -39,6 +41,11 @@ class ClientConnection {
     private final    Channel              channel;
     private volatile Protocol.ClientStage stage;
 
+    /**
+     * Creates a new connection handler for the joining channel stream
+     * 
+     * @param channelContext the channel of the player joining
+     */ 
     public ClientConnection(ChannelHandlerContext channelContext) {
         this.address = (InetSocketAddress) channelContext.channel().remoteAddress();
         this.channel = channelContext.channel();
@@ -46,14 +53,31 @@ class ClientConnection {
         ClientConnection.clientData.put(this.address, this);
     }
 
+    /**
+     * Checks if an IP address is logged into the server
+     * 
+     * @param address the address to check if online
+     * @return {@code true} if the IP is on the server, {@code false} if not
+     */ 
     public static boolean isLoggedIn(InetSocketAddress address) {
         return ClientConnection.clientData.containsKey(address);
     }
 
+    /**
+     * Gets the connection by the IP address
+     * 
+     * @param address the IP to lookup the connection handler
+     * @return the instnace of the client handler associated with the IP
+     */ 
     public static ClientConnection getConnection(InetSocketAddress address) {
         return ClientConnection.clientData.get(address);
     }
 
+    /**
+     * Sends protocol data through the client stream
+     * 
+     * @param packet the packet to send, encoded and written to the stream
+     */ 
     public void sendPacket(Packet packet) {
         // Create new ByteBuf
         ByteBuf buffer = this.channel.alloc().buffer();
@@ -69,24 +93,47 @@ class ClientConnection {
         ClientConnection.clientData.put(this.address, this);
     }
 
+    /**
+     * Gets the channel context for the connection stream
+     * 
+     * @return the netty channel wrapped by the handler
+     */ 
     public Channel getChannel() {
         return this.channel;
     }
 
+    /**
+     * The IP address of the player handled by this connection wrapper
+     * 
+     * @return the handled IP address
+     */ 
     public InetSocketAddress getAddress() {
         return this.address;
     }
 
+    /**
+     * Gets the current state of the connection
+     * 
+     * @return the current state of the protocol for the player
+     */ 
     public Protocol.ClientStage getStage() {
         return this.stage;
     }
 
+    /**
+     * Sets the player state, should only be used by the ClientConnectionHandler
+     * 
+     * @param stage the state to set the player to
+     */ 
     public void setStage(Protocol.ClientStage stage) {
         this.stage = stage;
 
         ClientConnection.clientData.put(this.address, this);
     }
 
+    /**
+     * Removes the player's server side client handler
+     */ 
     public void logout() {
         // TODO
         ClientConnection.clientData.remove(this.address);
