a254a47512985d6ad410f9d4d6e1918dd5ea0098
AgentTroll <woodyc40@gmail.com>
2015-01-10 14:08:31 -0800

    Licensing and test update

--- src/test/java/net/tridentsdk/server/bench/Benchmarks.java
+++ src/test/java/net/tridentsdk/server/bench/Benchmarks.java
@@ -1,3 +1,19 @@
+/*
+ * Trident - A Multithreaded Server Alternative
+ * Copyright 2014 The TridentSDK Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.tridentsdk.server.bench;
 
 import com.google.common.collect.LinkedListMultimap;
@@ -76,11 +92,11 @@ public class Benchmarks {
         String[] keys = asString(data.asMap().keySet().toArray());
         double[] base = asDouble(data.get("control").toArray());
 
-        StringBuilder builder = new StringBuilder("http://chart.googleapis.com/chart?").append(
-                "cht=lxy")                             // Chart type: Line/xy
+        StringBuilder builder = new StringBuilder("http://chart.googleapis.com/chart?")
+                .append("cht=lxy")                             // Chart type: Line/xy
                 .append("&chtt=Benchmark+Results:+")           // Chart title: Benchmark Results:
                 .append(what)                                  // specified
-                .append(" (ns/op)").append("&chs=900x300")                        // Chart size: 900x300
+                .append(" (ns/op)").append("&chs=900x300")     // Chart size: 900x300
                 .append("&chxt=x,x,y,y")                       // Chart x/y: visible
                 .append("&chxl=1:|CPU+Backoff|3:|Nanoseconds") // Axis labels
                 .append("&chds=a").append("&chxl=1:|CPU+Backoff|3:|Nanoseconds") // Axis labels
