00357ffded3d0d844799acf34dabf048ad65d1a4
AgentTroll <woodyc40@gmail.com>
2015-07-29 21:03:21 -0700

    improve client FPS stability

--- src/main/java/net/tridentsdk/server/player/TridentPlayer.java
+++ src/main/java/net/tridentsdk/server/player/TridentPlayer.java
@@ -27,6 +27,7 @@ import net.tridentsdk.Trident;
 import net.tridentsdk.base.Block;
 import net.tridentsdk.base.Position;
 import net.tridentsdk.base.Substance;
+import net.tridentsdk.config.ConfigSection;
 import net.tridentsdk.docs.InternalUseOnly;
 import net.tridentsdk.entity.Entity;
 import net.tridentsdk.entity.living.Player;
@@ -68,8 +69,9 @@ import static net.tridentsdk.server.packets.play.out.PacketPlayOutPlayerListItem
 public class TridentPlayer extends OfflinePlayer {
     private static final Map<UUID, Player> ONLINE_PLAYERS = new ConcurrentHashMap<>();
     private static final int MAX_VIEW = Trident.config().getInt("view-distance", 15);
-    private static final int MAX_CHUNKS = (int) Trident.config().getConfigSection("performance")
-            .getInt("max-chunks-player", 441);
+    private static final ConfigSection tridentCfg = Trident.config().getConfigSection("performance");
+    private static final int MAX_CHUNKS = tridentCfg.getInt("max-chunks-player", 441);
+    private static final int CLEAN_ITERATIONS = tridentCfg.getInt("chunk-clean-iterations-player", 2);
 
     private final PlayerConnection connection;
     private final Set<ChunkLocation> knownChunks = Sets.newConcurrentHashSet();
@@ -250,7 +252,7 @@ public class TridentPlayer extends OfflinePlayer {
         if (!chunkQueue.isEmpty())
             connection.sendPacket(chunkQueue.poll());
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < CLEAN_ITERATIONS; i++) {
             if (knownChunks.size() > MAX_CHUNKS) {
                 cleanChunks(distance - i);
             } else break;
@@ -261,24 +263,21 @@ public class TridentPlayer extends OfflinePlayer {
     }
 
     public void cleanChunks(int viewDist) {
-        int toClean = knownChunks.size() - MAX_CHUNKS;
-        if (toClean > 0) {
-            Position pos = position();
-            int x = (int) pos.x() / 16;
-            int z = (int) pos.z() / 16;
+        Position pos = position();
+        int x = (int) pos.x() / 16;
+        int z = (int) pos.z() / 16;
 
-            for (ChunkLocation location : knownChunks) {
-                int cx = location.x();
-                int cz = location.z();
+        for (ChunkLocation location : knownChunks) {
+            int cx = location.x();
+            int cz = location.z();
 
-                int abs = Math.abs(cx - x);
-                int abs1 = Math.abs(cz - z);
+            int abs = Math.abs(cx - x);
+            int abs1 = Math.abs(cz - z);
 
-                if (abs > viewDist || abs1 > viewDist) {
-                    ((TridentWorld) world()).loadedChunks.tryRemove(location);
-                    connection.sendPacket(new PacketPlayOutChunkData(new byte[0], location, true, (short) 0));
-                    knownChunks.remove(location);
-                }
+            if (abs > viewDist || abs1 > viewDist) {
+                ((TridentWorld) world()).loadedChunks.tryRemove(location);
+                connection.sendPacket(new PacketPlayOutChunkData(new byte[0], location, true, (short) 0));
+                knownChunks.remove(location);
             }
         }
     }
