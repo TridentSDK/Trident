6e1d5b9bf8f394ce0372274af11e15e360caec19
Vilsol <vilsol2000@gmail.com>
2016-08-26 00:38:07 +0300

    Generate a full report URL instead of static one

--- src/main/java/net/tridentsdk/server/TridentMain.java
+++ src/main/java/net/tridentsdk/server/TridentMain.java
@@ -27,7 +27,9 @@ import net.tridentsdk.server.config.ServerConfig;
 import net.tridentsdk.server.net.NetServer;
 
 import javax.annotation.concurrent.Immutable;
-import java.io.PrintStream;
+import java.io.*;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.nio.file.Files;
 import java.util.List;
 
@@ -45,10 +47,50 @@ public class TridentMain {
         try {
             start(args);
         } catch (Exception e) {
-            PrintStream o = System.out;
+            String url = "https://tridentsdk.atlassian.net/secure/CreateIssue!default.jspa";
+
+            try {
+                StackTraceElement element = e.getStackTrace()[0];
+                int pos = 0;
+                while(element.getClassName().startsWith("java")){
+                    element = e.getStackTrace()[pos++];
+                }
+
+                String errorMessage = e.getMessage() == null ? "java.lang.NullPointerException" : e.getMessage();
+                String summary = errorMessage + " in " + element.getClassName() + " at line " + element.getLineNumber();
+
+                StringWriter sw = new StringWriter();
+                PrintWriter pw = new PrintWriter(sw);
+                e.printStackTrace(pw);
+
+                String description = "{code:title=StackTrace}" + sw.toString() + "{code}";
+
+                String environment = "Trident Version: " + "0.5-alpha\n" + // TODO Get actual TridentSDK version
+                        "Operating System: " + System.getProperty("os.name") + " (" + System.getProperty("os.version") + ")\n" +
+                        "System Architecture: " + System.getProperty("os.arch") + "\n" +
+                        "Java Version: " + System.getProperty("java.version") + " (" + System.getProperty("java.vendor") + ")";
+
+                String longUrl = "https://tridentsdk.atlassian.net/secure/CreateIssueDetails!init.jspa?pid=10200&issuetype=1&priority=4&summary=" +
+                        URLEncoder.encode(summary, "UTF-8") +
+                        "&description=" +
+                        URLEncoder.encode(description, "UTF-8") +
+                        "&environment=" +
+                        URLEncoder.encode(environment, "UTF-8");
+
+                URL shortened = new URL("http://tsdk.xyz/api/v2/action/shorten?url=" + URLEncoder.encode(longUrl, "UTF-8"));
+                BufferedReader in = new BufferedReader(new InputStreamReader(shortened.openStream()));
+                url = in.readLine();
+            }catch (Exception ignored){
+            }
+
+
+            PrintStream o = System.err;
+            o.println();
+            o.println();
             o.println("Unhandled exception occurred while starting the server.");
             o.println("This was not intended to happen.");
-            o.println("Please report this on https://tridentsdk.atlassian.net/secure/CreateIssue!default.jspa");
+            o.println("Please report this on " + url);
+            o.println();
             e.printStackTrace();
 
             System.exit(1);
