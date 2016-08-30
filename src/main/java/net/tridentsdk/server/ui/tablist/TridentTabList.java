8fe7a3e7d6cba17142a9bfd25188f14da3efb115
Vilsol <vilsol2000@gmail.com>
2016-08-30 16:43:29 +0300

    Fix variables that "don't do anything"

--- src/main/java/net/tridentsdk/server/ui/tablist/TridentTabList.java
+++ src/main/java/net/tridentsdk/server/ui/tablist/TridentTabList.java
@@ -1,3 +1,19 @@
+/*
+ * Trident - A Multithreaded Server Alternative
+ * Copyright 2016 The TridentSDK Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.tridentsdk.server.ui.tablist;
 
 import net.tridentsdk.chat.ChatComponent;
@@ -12,10 +28,10 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 public abstract class TridentTabList implements TabList {
 
-    private List<Player> users;
+    private final List<Player> users;
     private ChatComponent header;
     private ChatComponent footer;
-    protected List<TabListElement> elements;
+    protected final List<TabListElement> elements;
 
     public TridentTabList() {
         this.users = new CopyOnWriteArrayList<>();
