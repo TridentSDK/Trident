9f71c55e0b2967f4e390e31282aaf6ce85a2c2a8
Nick Robson <robson.nicholas1@gmail.com>
2016-09-01 14:16:28 +1000

    Take render distance from settings packet

--- src/main/java/net/tridentsdk/server/packet/play/PlayInClientSettings.java
+++ src/main/java/net/tridentsdk/server/packet/play/PlayInClientSettings.java
@@ -2,10 +2,10 @@ package net.tridentsdk.server.packet.play;
 
 import io.netty.buffer.ByteBuf;
 import net.tridentsdk.chat.ClientChatMode;
-import net.tridentsdk.entity.living.Player;
 import net.tridentsdk.server.net.NetClient;
 import net.tridentsdk.server.net.NetData;
 import net.tridentsdk.server.packet.PacketIn;
+import net.tridentsdk.server.player.TridentPlayer;
 
 /**
  * @author TridentSDK
@@ -20,13 +20,14 @@ public class PlayInClientSettings extends PacketIn {
     @Override
     public void read(ByteBuf buf, NetClient client) {
         String locale = NetData.rstr(buf);
-        byte viewDistance = buf.readByte();
+        byte renderDistance = buf.readByte();
         ClientChatMode chatMode = ClientChatMode.of(NetData.rvint(buf));
         boolean chatColors = buf.readBoolean();
         byte skinFlags = buf.readByte();
         int mainHand = buf.readByte();
 
-        Player player = client.player();
+        TridentPlayer player = client.player();
+        player.setRenderDistance(renderDistance);
         player.getMetadata().setSkinFlags(skinFlags);
         player.getMetadata().setLeftHandMain(mainHand == 0);
         player.updateMetadata();
