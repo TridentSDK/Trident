5212473e43982abb961b9a78a6219bba05ae3481
AgentTroll <woodyc40@gmail.com>
2017-02-24 17:21:56 -0800

    Remove command prompt

--- src/main/java/net/tridentsdk/server/command/DefaultLogger.java
+++ src/main/java/net/tridentsdk/server/command/DefaultLogger.java
@@ -16,6 +16,8 @@
  */
 package net.tridentsdk.server.command;
 
+import org.fusesource.jansi.AnsiConsole;
+
 import javax.annotation.concurrent.Immutable;
 import java.io.OutputStream;
 import java.io.PrintStream;
@@ -42,7 +44,7 @@ public class DefaultLogger extends PipelinedLogger {
      */
     public DefaultLogger() {
         super(null);
-        this.stream = new TridentPrintStream();
+        this.stream = AnsiConsole.out;
 
         System.setOut(this.stream);
         System.setErr(this.stream);
