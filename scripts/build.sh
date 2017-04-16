8be8d0ee0747492e1d89d1ee3cb8d55ea262aff6
Vilsol <Vilsol@users.noreply.github.com>
2017-04-16 07:38:09 +0300

    Checkout new branch

--- scripts/build.sh
+++ scripts/build.sh
@@ -23,6 +23,15 @@ else
 fi
 
 pushd TridentSDK
+
+REMOTE_REPO=$(git remote -v | grep fetch | awk '{print $2}')
+BRANCH_EXISTS=$($(git ls-remote --heads $REMOTE_REPO $TRAVIS_BRANCH))
+
+if [ "$BRANCH_EXISTS" ];
+then
+    git checkout $TRAVIS_BRANCH
+fi
+
 mvn clean install
 popd
 
