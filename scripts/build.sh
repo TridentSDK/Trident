4f415577bfa7ae5c853f8896f68ad406467d3a76
Vilsol <Vilsol@users.noreply.github.com>
2017-04-16 07:08:19 +0300

    Add pull request logic

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,26 @@
 echo "Currently on branch: $TRAVIS_BRANCH"
+echo "Pull request: $TRAVIS_PULL_REQUEST"
 
-git clone -b revamp https://github.com/TridentSDK/TridentSDK.git
+if [ "$TRAVIS_PULL_REQUEST" != "false" ];
+then
+    FULL_SLUG=$TRAVIS_PULL_REQUEST_SLUG + "SDK"
+    
+    echo $FULL_SLUG
+    
+    OWN_REPO=$(curl --write-out %{http_code} --silent --output /dev/null "https://github.com/$FULL_SLUG")
+    
+    echo "Own SDK Repo: $OWN_REPO"
+    
+    if [ "$OWN_REPO" != "404" ];
+    then
+        git clone -b revamp "https://github.com/$FULL_SLUG.git"
+    else
+        git clone -b revamp https://github.com/TridentSDK/TridentSDK.git
+    fi
+else
+    git clone -b revamp https://github.com/TridentSDK/TridentSDK.git
+fi
+
 pushd TridentSDK
 mvn clean install
 popd
