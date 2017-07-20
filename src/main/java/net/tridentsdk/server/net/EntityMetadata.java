f2485bf2b59faf929e21bd020dc08ee0266e8839
Nick Robson <robson.nicholas1@gmail.com>
2017-07-20 16:29:15 +1000

    Fix encoding/decoding of entity metadata

--- src/main/java/net/tridentsdk/server/net/EntityMetadata.java
+++ src/main/java/net/tridentsdk/server/net/EntityMetadata.java
@@ -19,6 +19,7 @@ package net.tridentsdk.server.net;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import io.netty.buffer.ByteBuf;
+import java.util.Objects;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import net.tridentsdk.base.BlockDirection;
@@ -38,6 +39,8 @@ import java.util.concurrent.atomic.AtomicReference;
  */
 @ThreadSafe
 public class EntityMetadata {
+    private static final Gson GSON = new Gson();
+
     private final List<EntityMetadata.EntityMetadataItem> items = Collections.synchronizedList(new ArrayList<>());
 
     public EntityMetadata.EntityMetadataItem get(int x) {
@@ -57,7 +60,7 @@ public class EntityMetadata {
         short id;
         while ((id = buf.readUnsignedByte()) != 0xFF) {
             EntityMetadata.EntityMetadataType type = EntityMetadata.EntityMetadataType.values()[id];
-            Object value = null;
+            Object value;
             switch (type) {
                 case BYTE:
                     value = buf.readByte();
@@ -72,7 +75,7 @@ public class EntityMetadata {
                     value = NetData.rstr(buf);
                     break;
                 case CHAT:
-                    value = ChatComponent.fromJson(new Gson().fromJson(NetData.rstr(buf), JsonObject.class));
+                    value = ChatComponent.fromJson(GSON.fromJson(NetData.rstr(buf), JsonObject.class));
                     break;
                 case SLOT:
                     value = Slot.read(buf);
@@ -116,10 +119,10 @@ public class EntityMetadata {
                     int bid = NetData.rvint(buf);
                     value = new int[]{ bid >> 4 & 0xF, bid & 0xF };
                     break;
+                default:
+                    continue;
             }
-            if (value != null) {
-                items.add(new EntityMetadata.EntityMetadataItem(id, type, new AtomicReference<>(value)));
-            }
+            items.add(new EntityMetadata.EntityMetadataItem(id, type, new AtomicReference<>(value)));
         }
         this.items.clear();
         this.items.addAll(items);
@@ -134,51 +137,60 @@ public class EntityMetadata {
 
             switch (item.type) {
                 case BYTE:
-                    buf.writeByte((byte) item.value.get());
+                    buf.writeByte(item.asByte());
                     break;
                 case VARINT:
-                    NetData.wvint(buf, (int) item.value.get());
+                    NetData.wvint(buf, item.asInt());
                     break;
                 case FLOAT:
-                    buf.writeFloat((float) item.value.get());
+                    buf.writeFloat(item.asFloat());
                     break;
                 case STRING:
-                    NetData.wstr(buf, item.value.toString());
+                    String str = Objects.requireNonNull(item.asString(), "string required but was null (idx: " + item.index + ")");
+                    NetData.wstr(buf, str);
+                    break;
+                case CHAT:
+                    ChatComponent cc = Objects.requireNonNull(item.asChatComponent(), "chat component required but was null (idx: " + item.index + ")");
+                    NetData.wstr(buf, cc.toString());
                     break;
                 case SLOT:
-                    ((Slot) item.value.get()).write(buf);
+                    Slot slot = Objects.requireNonNull(item.asSlot(), "slot required but was null (idx: " + item.index + ")");
+                    slot.write(buf);
                     break;
                 case BOOLEAN:
-                    buf.writeBoolean((Boolean) item.value.get());
+                    buf.writeBoolean(item.asBoolean());
                     break;
                 case ROTATION:
-                    Vector rv = (Vector) item.value.get();
+                    Vector rv = Objects.requireNonNull(item.asRotation(), "rotation required but was null (idx: " + item.index + ")");
                     buf.writeFloat((float) rv.getX());
                     buf.writeFloat((float) rv.getY());
                     buf.writeFloat((float) rv.getZ());
                     break;
                 case POSITION:
-                    Vector pv = (Vector) item.value.get();
+                    Vector pv = Objects.requireNonNull(item.asPosition(), "position required but was null (idx: " + item.index + ")");
                     NetData.wvec(buf, pv);
                     break;
                 case OPTPOSITION:
-                    Vector opv = (Vector) item.value.get();
+                    Vector opv = item.asPosition();
+                    buf.writeBoolean(opv != null);
                     if (opv != null) {
                         NetData.wvec(buf, opv);
                     }
                     break;
                 case DIRECTION:
-                    NetData.wvint(buf, ((BlockDirection) item.value.get()).getMinecraftDirection());
+                    BlockDirection direction = Objects.requireNonNull(item.asDirection(), "direction required but was null (idx: " + item.index + ")");
+                    NetData.wvint(buf, direction.getMinecraftDirection());
                     break;
                 case OPTUUID:
-                    UUID uuid = (UUID) item.value.get();
+                    UUID uuid = item.asUUID();
+                    buf.writeBoolean(uuid != null);
                     if (uuid != null) {
                         buf.writeLong(uuid.getMostSignificantBits());
                         buf.writeLong(uuid.getLeastSignificantBits());
                     }
                     break;
                 case BLOCKID:
-                    int[] blockIdData = (int[]) item.value.get();
+                    int[] blockIdData = item.asBlockId();
                     NetData.wvint(buf, blockIdData[0] << 4 | blockIdData[1]);
                     break;
             }
@@ -188,7 +200,7 @@ public class EntityMetadata {
 
     @Getter
     @AllArgsConstructor
-    public static final class EntityMetadataItem {
+    public static class EntityMetadataItem {
         private final int index;
         private final EntityMetadata.EntityMetadataType type;
         private final AtomicReference<Object> value;
@@ -198,11 +210,18 @@ public class EntityMetadata {
         }
 
         public byte asByte() {
-            return (byte) this.value.get();
+            Object object = this.value.get();
+            return object instanceof Number ? ((Number) object).byteValue() : 0;
         }
 
         public int asInt() {
-            return (int) this.value.get();
+            Object object = this.value.get();
+            return object instanceof Number ? ((Number) object).intValue() : 0;
+        }
+
+        public float asFloat() {
+            Object object = this.value.get();
+            return object instanceof Number ? ((Number) object).floatValue() : 0;
         }
 
         public boolean asBit(int x) {
@@ -213,10 +232,9 @@ public class EntityMetadata {
             byte val;
             byte newVal;
             do {
-                val = newVal = (byte) this.value.get();
+                val = newVal = this.asByte();
                 if ((val & 1 << x) != 0 == value)
                     return;
-
                 if (value) {
                     newVal |= 1 << x;
                 } else {
@@ -225,12 +243,9 @@ public class EntityMetadata {
             } while (!this.value.compareAndSet(val, newVal));
         }
 
-        public float asFloat() {
-            return (float) this.value.get();
-        }
-
         public String asString() {
-            return (String) this.value.get();
+            Object object = this.value.get();
+            return String.valueOf(object);
         }
 
         public ChatComponent asChatComponent() {
@@ -238,7 +253,12 @@ public class EntityMetadata {
         }
 
         public boolean asBoolean() {
-            return (boolean) this.value.get();
+            Object object = this.value.get();
+            return object instanceof Boolean ? (Boolean) object : Boolean.valueOf(String.valueOf(object));
+        }
+
+        public Slot asSlot() {
+            return (Slot) this.value.get();
         }
 
         public Vector asRotation() {
@@ -266,43 +286,44 @@ public class EntityMetadata {
         BYTE(0) {
             @Override
             public Object cast(Object object) {
-                return ((Number) object).byteValue();
+                return object == null ? 0 : ((Number) object).byteValue();
             }
         },
         VARINT(1) {
             @Override
             public Object cast(Object object) {
-                return ((Number) object).intValue();
+                return object == null ? 0 : ((Number) object).intValue();
             }
         },
         FLOAT(2) {
             @Override
             public Object cast(Object object) {
-                return ((Number) object).floatValue();
+                return object == null ? 0 : ((Number) object).floatValue();
             }
         },
         STRING(3) {
             @Override
             public Object cast(Object object) {
-                return object.toString();
+                return object == null ? "" : object.toString();
             }
         },
         CHAT(4) {
             @Override
             public Object cast(Object object) {
-                return object.toString();
+                return object == null ? new ChatComponent() : object.toString();
             }
         },
         SLOT(5) {
             @Override
             public Object cast(Object object) {
-                return null;
+                // TODO handle slot casting from NBT and JSON where applicable
+                return object;
             }
         },
         BOOLEAN(6) {
             @Override
             public Object cast(Object object) {
-                return object instanceof Boolean ? (boolean) object : Boolean.parseBoolean(object.toString());
+                return object instanceof Boolean ? (boolean) object : Boolean.parseBoolean(String.valueOf(object));
             }
         },
         ROTATION(7) {
@@ -350,4 +371,5 @@ public class EntityMetadata {
 
         public abstract Object cast(Object object);
     }
+
 }
