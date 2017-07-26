de51fdfc1ad5b3c488e2f3c210c8b4b6b2f68901
Nick Robson <robson.nicholas1@gmail.com>
2017-07-26 14:27:06 +1000

    Do the reverty thing

--- scripts/build.sh
+++ scripts/build.sh
@@ -38,7 +38,17 @@ then
     git checkout $ORIGIN_BRANCH
 fi
 
-./gradlew clean install
+mvn clean install
 popd
 
-./gradlew clean install
+if [ "$TRAVIS_BRANCH" == "revamp" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ];
+then
+    echo "Compiling with deployment"
+    mvn clean install deploy --settings travis/settings.xml
+
+    # Ping docker hub
+    curl -H "Content-Type: application/json" --data "'{\"build\": true}'" -X POST https://registry.hub.docker.com/u/tridentsdk/trident/trigger/${DOCKER_TRIGGER_TOKEN}/
+else
+    echo "Compiling without deployment"
+    mvn clean install --settings travis/settings.xml
+fi
